b8c2cd488e740979a8a72e64e9db125e8aa13c6f
Philipp Andreas <philipp.andreas@vanzoo.de>
2018-12-04 13:11:58 +0100

    Clean shutdown

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ test -e "/data/graphite/storage/graphite.db" && echo "graphite.db already exists
 
 cd /var/lib/graphite/conf
 
-gunicorn -b 0.0.0.0:8000 -w 2 graphite_wsgi:application
+gunicorn -b 0.0.0.0:8000 -w 2 graphite_wsgi:application &
+PID=$!
+trap 'while kill $PID > /dev/null 2>&1;do sleep 1;done;exit 0' SIGTERM
+while true; do sleep 10; done
